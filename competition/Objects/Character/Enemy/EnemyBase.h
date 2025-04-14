a9631326524bca9c6d15bd0818ccd9cec8b84603
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2025-04-14 10:04:17 +0900

    a

--- competition/Objects/Character/Enemy/EnemyBase.h
+++ competition/Objects/Character/Enemy/EnemyBase.h
@@ -6,6 +6,18 @@ class EnemyBase : public Character
 {
 protected:
 
+
+public:
+    virtual void Initialize() override;
+    virtual void Update(float) override;
+    virtual void Draw(const Vector2D&, bool) const override;
+    virtual void Finalize() override;
+
+    virtual void OnHitCollision(GameObject*) override;
+
+public:
+    void Movement(float);
+    void Animation();
 private:
 
 };
