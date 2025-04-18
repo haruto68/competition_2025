0e5e23f3a7fabe2e1fbac977b88ea718c21231d4
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2025-04-18 17:48:20 +0900

    やらかし

--- competition/Objects/Character/Enemy/Enemy1/Enemy1.cpp
+++ competition/Objects/Character/Enemy/Enemy1/Enemy1.cpp
@@ -22,6 +22,15 @@ void Enemy1::Update(float delta_seconds)
 	Movement(delta_seconds);
 	Animation();
 
+	//入力機能インスタンス取得
+	InputManager* input = InputManager::GetInstance();
+
+	if (CheckHitKey(KEY_INPUT_V))
+	{
+		EnemyShot* shot = object_manager->CreateGameObject<EnemyShot>(this->location);
+		shot->SetShotType(eEnemy3);
+	}
+
 }
 
 void Enemy1::Draw(const Vector2D& screeen_offset, bool file_flag) const
@@ -47,11 +56,11 @@ void Enemy1::Movement(float delta_seconds)
 {
 	/*float speed = 200.0f;*/
 	//location.y -= 1.0f * speed * delta_seconds;
-	location.y -= 1.0f*speed*delta_seconds;
-	if (location.y <= 0||location.y>=720)
+	location.y -= 1.0f * speed * delta_seconds;
+	if (location.y <= 0 || location.y >= 720)
 	{
 		speed *= -1;
-		
+
 	}
 }
 
