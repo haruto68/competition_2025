341d2e1afe97f44a67bac926cd027ac4270363dc
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2025-05-07 11:49:27 +0900

    ヘッダー、CPPの名前変更,ヘッダー追加

--- competition/Scenes/UI/HpUi.h
+++ competition/Scenes/UI/HpUi.h
@@ -1,1 +1,18 @@
 #pragma once
+#include"../../Utility/Singleton.h"
+#include"../../Resource/ResourceManager.h"
+
+class HpUI
+{
+private:
+
+public:
+	HpUI();
+	~HpUI();
+
+public:
+	void Initialize();			//‰Šú‰»ˆ—
+	void Update(bool flag);		//XVˆ—
+	void Draw() const;			//•`‰æˆ—
+	void Finalize();			//I—¹ˆ—
+};
