49420790c4b9cc1fc73fcbfbf6b8c4569b6a3b33
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2025-05-08 15:00:41 +0900

    敵出現

--- competition/Scenes/InGameScene/InGameScene.cpp
+++ competition/Scenes/InGameScene/InGameScene.cpp
@@ -42,6 +42,7 @@ InGameScene::~InGameScene()
 
 void InGameScene::Initialize()
 {
+
 	//GameObjectManagerインスタンス取得
 	object_manager = new GameObjectManager();
 
@@ -245,34 +246,46 @@ void InGameScene::Spawn()        //敵の自動生成
 
 	int num = rand() % 100 + 1;
 	
-	if (num <= 90)
+	if (num <= 95)
 	{
 
 		EnemyBase* enemy;
 		switch (ramdom_r)
 		{
 		case 0:
-			enemy = object_manager->CreateGameObject<Enemy1>(Vector2D(1300, Y_b));
+			enemy = object_manager->CreateGameObject<Enemy1>(Vector2D(1300, 400));//ジグザグ
 			break;
 		case 1:
-			enemy = object_manager->CreateGameObject<Enemy2>(Vector2D(1300, Y_b));
+			enemy = object_manager->CreateGameObject<Enemy2>(Vector2D(1300, 680));//大砲
 			break;
 		case 2:
-			enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, Y_b));
+			enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 400));//特攻、真ん中
+			break;
+		case 3:
+			enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 200));//特攻、上
+			break;
+		case 4:
+			enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 600));//特攻、下
 			break;
 		default:
 			break;
 		}
 	}
 
-	/*if (CheckHitKey(KEY_INPUT_1)) {
-		auto enemy = object_manager->CreateGameObject<Enemy1>(Vector2D(1300, Y_b));
+	/*if (CheckHitKey(KEY_INPUT_0)) {
+		auto enemy = object_manager->CreateGameObject<Enemy1>(Vector2D(1300, 400));
+	}
+	else if (CheckHitKey(KEY_INPUT_1)) {
+		auto enemy = object_manager->CreateGameObject<Enemy2>(Vector2D(1300, 680));
 	}
 	else if (CheckHitKey(KEY_INPUT_2)) {
-		auto enemy = object_manager->CreateGameObject<Enemy2>(Vector2D(1300, Y_b));
+		auto enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 400));
 	}
 	else if (CheckHitKey(KEY_INPUT_3)) {
-		auto enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, Y_b));
+		auto enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 200));
+	}
+	else if (CheckHitKey(KEY_INPUT_4)) {
+		auto enemy = object_manager->CreateGameObject<Enemy3>(Vector2D(1300, 600));
 	}*/
 
 
