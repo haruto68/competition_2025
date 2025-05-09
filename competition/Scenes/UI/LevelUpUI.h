c50abf1d194006df539439c38a0ad507bf8380b7
haruto68 <138412489+haruto68@users.noreply.github.com>
2025-05-09 15:03:09 +0900

    LevelUp_0509

--- competition/Scenes/UI/LevelUpUI.h
+++ competition/Scenes/UI/LevelUpUI.h
@@ -2,6 +2,13 @@
 #include"../../Utility/Singleton.h"
 #include"../../Resource/ResourceManager.h"
 #include"../../Utility/InputManager.h"
+#include"../../Objects/Character/Player/PlayerStats.h"
+
+#define	LOT_MAX			(3)	//最大抽選数
+
+#define	LOT_HP			(0)
+#define LOT_DAMAGE		(1)
+#define LOT_SPEED		(2)
 
 class LevelUpUI
 {
@@ -10,13 +17,13 @@ public:
 	int cursor;
 private:
 	//画像
-	int window[3] = {};
+	int window[4] = {};
 	int power[5] = {};
 	int power_icon[5] = {};
 
-
-	int lottery[3] = {};		//強化内容抽選
-	int lottery_icon[3] = {};	//強化内容アイコン
+	ePowerUp lot[LOT_MAX] = {};			//強化内容抽選
+	int lot_str[LOT_MAX] = {};			//強化内容文字
+	int lot_icon[LOT_MAX] = {};			//強化内容アイコン
 
 
 public:
@@ -24,8 +31,11 @@ public:
 	~LevelUpUI();
 
 public:
-	void Initialize();			//初期化処理
-	void Update(bool flag);		//更新処理
-	void Draw() const;			//描画処理
-	void Finalize();			//終了処理
+	void Initialize();					//初期化処理
+	void Update(bool flag);				//更新処理
+	void Draw(PlayerStats stats) const;	//描画処理
+	void Finalize();					//終了処理
+
+	void Lottery(int* nums);	//抽選
+	ePowerUp GetLottery();				//抽選内容取得
 };
