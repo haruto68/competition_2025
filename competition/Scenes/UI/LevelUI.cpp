0dbe57fe2fb36895305774cd49a8887f536112ee
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2025-05-15 12:00:46 +0900

    経験値量の可視化の追加

--- competition/Scenes/UI/LevelUI.cpp
+++ competition/Scenes/UI/LevelUI.cpp
@@ -6,7 +6,6 @@ LevelUI::LevelUI() :
 	max_exp(0),
 	level_ui1(0),
 	level_ui2(0),
-	level_ui3(0),
 	level(0)
 {
 	// リソース管理インスタンス取得
@@ -14,7 +13,6 @@ LevelUI::LevelUI() :
 	// level_bar = rm->GetImages("Resource/Images/LevelUi/level_ui3.png", 10, 10, 1, 32, 32);
 	level_ui1 = rm->GetImages("Resource/Images/LevelUi/level_ui1.png")[0];
 	level_ui2 = rm->GetImages("Resource/Images/LevelUi/level_ui2.png")[0];
-	level_ui3 = rm->GetImages("Resource/Images/LevelUi/level_ui3.png")[0];
 }
 
 LevelUI::~LevelUI()
@@ -29,23 +27,25 @@ void LevelUI::Initialize()
 
 void LevelUI::Update()
 {
+	// max経験値の割合(現在の経験値量に対しての)
 	level = (exp * 100) / max_exp;
 }
 
 void LevelUI::Draw() const
 {
 	// テスト用描画
-	DrawRotaGraph(30, 700, 0.9, 0, level_ui1, 1, 0);
-	for (int i = 0; i < exp; i++)			
+	if (level != 0)
 	{
-		// DrawRotaGraph((70 + (i * 40)), 700, 0.9, 0, level_ui2, 1, 0);	// max描画は31(画面左から右端まで 右端が見えるのは30)
-		// DrawRotaGraph((90 + (i * 80)), 700, 0.9, 0, level_ui3, 1, 0);	// max描画は16(画面左から右端まで 右端が見えるのは15)
+		DrawRotaGraph(30, 700, 0.9, 0, level_ui1, 1, 0);
+	}
+	for (int i = 0; i < level; i++)			
+	{
+		DrawRotaGraph((70 + (i * 12)), 700, 0.9, 0, level_ui2, 1, 0);	// max描画は31(画面左から右端まで 右端が見えるのは30)
 	}
 
 	// DrawFormatString(640, 80, 0xff00ff, "%d", exp);
-	DrawFormatString(640, 80, 0xff00ff, "%d", level);
+	// DrawFormatString(640, 80, 0xff00ff, "%d", level);
 	// DrawRotaGraph(58, 700, 0.9, 0, level_bar[1], 1, 0);
-	// DrawRotaGraph(640, 80, 1.0, 0, window[1], 1, 0);
 
 }
 
