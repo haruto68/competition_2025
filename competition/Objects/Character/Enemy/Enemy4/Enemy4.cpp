0a0f0146266f6138a1a307c5fd601769000a37d2
Amotoki0622 <138412674+Amotoki0622@users.noreply.github.com>
2025-05-27 15:35:25 +0900

    敵4の音源の追加

--- competition/Objects/Character/Enemy/Enemy4/Enemy4.cpp
+++ competition/Objects/Character/Enemy/Enemy4/Enemy4.cpp
@@ -18,6 +18,9 @@ Enemy4::Enemy4()
 	// ‰Â“®«Ý’è
 	is_mobility = true;
 
+	// ‰¹Œ¹Žæ“¾(0: “G‚ª”j‰óŽž‚Ì‰¹ 1: “G‚ª’e‚ðŒ‚‚Á‚½Žž‚Ì‰¹)
+	soundseffect[0] = rm->GetSounds("Resource/Sounds/SoundsEffect/Enemy/enemybreak.mp3");
+	soundseffect[1] = rm->GetSounds("Resource/Sounds/SoundsEffect/Enemy/enemyshot.mp3");
 }
 
 Enemy4::~Enemy4()
@@ -114,6 +117,7 @@ void Enemy4::OnHitCollision(GameObject* hit_object)
 	{
 		object_manager->CreateGameObject< ExperiencePoints>(this->location);
 		object_manager->DestroyGameObject(this);
+		PlaySoundMem(soundseffect[0], DX_PLAYTYPE_BACK, TRUE);
 	}
 }
 
